231bc7d76cdb32e074df8688124499400b223341
aquinteros <alvaro.quinteros.a@gmail.com>
2023-08-10 17:21:10 -0400

    allow js in header

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ from streamlit_extras.mention import mention
 def get_html(url):
 	"""Get the html of a url"""
 	headers = {
-		"User-Agent": "Edge/12.10240",
+		"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+		"Accept": "application/json, text/javascript, */*; q=0.01",
 		"method": "GET",
 		"mode": "cors",
 	}
